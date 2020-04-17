0ec73ec72520f563c19c55a53b882b9d0cf4484f
Naveen <navegoya@in.ibm.com>
2020-04-17 17:10:18 +0530

    x

--- src/main/java/z_5_TestMySQLConnection/z_5_TestMySQLConnection/MysqlCon.java
+++ src/main/java/z_5_TestMySQLConnection/z_5_TestMySQLConnection/MysqlCon.java
@@ -12,7 +12,7 @@ public class MysqlCon {
 		try{  
 			Class.forName("com.mysql.jdbc.Driver");  
 			Connection con=DriverManager.getConnection(  
-					"jdbc:mysql://mysql:3306/sampledb?user=root&password=WHbQPe3gSietbBSK");  
+					"jdbc:mysql://mysql:3306/sampledb?user=root&password=hDgjFKdKO8avDhBt");  
 			//here sonoo is database name, root is username and password  
 			Statement stmt=con.createStatement();  
 			ResultSet rs=stmt.executeQuery("select database()");  
